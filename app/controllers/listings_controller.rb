36aa65f77846d735c422262ced2e77ebba5a92cb
Ross Baker <rossbaker32@gmail.com>
2020-04-27 10:20:55 +1000

    added select for breed and radio for sex

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -12,12 +12,14 @@ class ListingsController < ApplicationController
 
     def new
         @listing = Listing.new
+        set_breeds_and_sexes
     end
 
     def create
         #finish logic for creating a record
         @listing = Listing.create(listing_params)
         if @listing.errors.any?
+            set_breeds_and_sexes
             render "new"
         end
     end
@@ -30,7 +32,6 @@ class ListingsController < ApplicationController
     end
 
     def destroy
-        
         #finish logic for deleting the record
     end
 
@@ -42,6 +43,11 @@ class ListingsController < ApplicationController
 
     def listing_params
         params.require(:listing).permit(:title, :description, :breed_id, :sex, :price, :deposit, :date_of_birth, :diet, :picture)
+    end
+
+    def set_breeds_and_sexes
+        @breeds = Breed.all
+        @sexes = Listing.sexes.keys
     end
 
 end
